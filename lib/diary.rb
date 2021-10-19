aa0936739e8f51ab39c1170ab648bf7072ed01f6
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-19 13:20:24 +0100

    Wrote test for diary to lock and passed function by returning true

--- lib/diary.rb
+++ lib/diary.rb
@@ -2,4 +2,8 @@ class Diary
   def lock
     true
   end
+
+  def unlock
+    true
+  end
 end
--- spec/features/diary_spec.rb
+++ spec/features/diary_spec.rb
@@ -6,4 +6,10 @@ describe Diary do
 
     expect(diary.lock).to eq true
   end
+
+  it "unlocks the diary so users can add or get entries" do
+    diary = Diary.new
+
+    expect(diary.unlock). to eq true
+  end
 end
